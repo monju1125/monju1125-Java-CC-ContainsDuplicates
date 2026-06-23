8e34dbc34e052fb94043eb8d0e0d9bf120a2a5ad
monju1125 <monju1125@github.com>
2026-06-23 06:53:40 +0000

    Test run: 4/4 passed, 0 failed, 0 skipped - 2026-06-23T06:53:40.245Z

--- src/main/java/ContainsDuplicates.java
+++ src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,14 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        int size = nums.length;
+        Set<Integer> mySet = new HashSet<Integer>();
+        for(int i = 0; i<size; i++){
+            if (mySet.contains(nums[i])) {
+                return true;
+            }
+            mySet.add(nums[i]);
+        }
         return false;
     }
 }
